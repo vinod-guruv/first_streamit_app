bdfadedf25a68f43be81b1d99e7166e9e869473b
Vinod G <110106645+vinod-guruv@users.noreply.github.com>
2023-10-11 16:16:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,15 +35,8 @@ try:
     if not fruit_choice:
         streamlit.error('Please select a fruit to get information.')
     else:
-            #streamlit.write('The user entered ', fruit_choice)
-          #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-          #streamlit.text(fruityvice_response.json())
-          # write your own comment -what does the next line do? 
-          #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-          # write your own comment - what does this do?
-          #streamlit.dataframe(fruityvice_normalized)
-            back_from_function = get_fruityvice_data(fruit_choice)
-            streamlit.dataframe(back_from_function)
+	back_from_function = get_fruityvice_data(fruit_choice)
+	streamlit.dataframe(back_from_function)
       
 streamlit.header("The fruit load list contains:")
 #Snowflake-related functins
